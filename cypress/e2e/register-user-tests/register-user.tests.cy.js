e9942744da81e9a6a254735ec46fd403e3fdb0f2
willen01 <willen.sesantos@gmail.com>
2025-10-21 15:17:39 -0300

    test: Adiciona automação para cadastro de novos clientes

---
+++ cypress/e2e/register-user-tests/register-user.tests.cy.js
@@ -0,0 +1,63 @@
+describe('validar registro de usuário', () => {
+    beforeEach(() => {
+        cy.visit('https://bearstore-testsite.smartbear.com/register');
+    });
+
+
+    it('Cadastro com campos obrigatórios vazios', () => {
+        cy.get('#FirstName').type('Jane')
+        cy.get('#LastName').type('Doe')    
+        cy.get('#DateOfBirthDay').select('5')
+        cy.get('#DateOfBirthMonth').select('August')        
+        cy.get('#DateOfBirthYear').select('1997')
+        cy.get('#Company').type('Company name')    
+
+        cy.get('#register-button').click()
+        cy.get('#Email-error').contains("\'Email'\ should not be empty.").should('be.visible')
+        cy.get('#Password-error').contains("\'Password'\ should not be empty.").should('be.visible')
+        cy.get("#ConfirmPassword-error").contains("\'Confirm password'\ should not be empty.").should('be.visible')
+
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/register')
+    })
+
+    it('Cadastro com email inválido', () => {
+        cy.get("#Email").type("jane.doe1.com")
+
+        cy.get('#register-button').click()
+        cy.get('#Email-error').contains("\'Email'\ is not a valid email address.").should('be.visible')
+
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/register')
+    })
+
+    
+    it('Cadastro com email já utilizado', () => {
+        cy.get("#Email").type("jhon.doe@provider.com")
+        cy.get('#Username').type("jhony")
+        cy.get("#Password").type("123456")
+        cy.get("#ConfirmPassword").type("123456")
+        cy.get('#register-button').click()
+        cy.contains('li', 'The specified email already exists')
+
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/register')
+    })
+
+
+/*
+    it('Cadastro com senha e confirmação de senha divergentes', () => {
+
+    })
+
+*/
+    /*
+    it('Validar processo de registro de novo cliente', () => {
+        cy.get('#FirstName').type('Jane')
+        cy.get('#LastName').type('Doe')    
+        cy.get('#DateOfBirthDay').select('5')
+        cy.get('#DateOfBirthMonth').select('August')        
+        cy.get('#DateOfBirthYear').select('1997')
+
+        cy.get('#register-button').click()
+    })
+    */
+
+})
