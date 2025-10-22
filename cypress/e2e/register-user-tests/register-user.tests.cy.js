1cf65a1f2e934779f655e0533b2e42055589227e
willen01 <willen.sesantos@gmail.com>
2025-10-22 10:00:19 -0300

    test: Adiciona teste de cadastro de usuário

--- cypress/e2e/register-user-tests/register-user.tests.cy.js
+++ cypress/e2e/register-user-tests/register-user.tests.cy.js
@@ -1,10 +1,13 @@
+import { faker } from "@faker-js/faker";
+
+
 describe('validar registro de usuário', () => {
     beforeEach(() => {
         cy.visit('https://bearstore-testsite.smartbear.com/register');
     });
 
 
-    it('Cadastro com campos obrigatórios vazios', () => {
+    it.skip('Cadastro com campos obrigatórios vazios', () => {
         cy.get('#FirstName').type('Jane')
         cy.get('#LastName').type('Doe')    
         cy.get('#DateOfBirthDay').select('5')
@@ -20,7 +23,7 @@ describe('validar registro de usuário', () => {
         cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/register')
     })
     
-    it('Cadastro de usuário com e-mail já cadastrado', () => {
+    it.skip('Cadastro de usuário com e-mail já cadastrado', () => {
         cy.get("#Email").type("jhon.doe@provider.com")
         cy.get('#Username').type("jhony")
         cy.get("#Password").type("123456")
@@ -30,5 +33,25 @@ describe('validar registro de usuário', () => {
 
         cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/register')
     })
+
+    it('Cadastro de usuário com dados válidos', () => {
+        cy.get("#FirstName").type("Jhon")
+        cy.get("#LastName").type("Doe")
+        cy.get('#DateOfBirthDay').select('5')
+        cy.get('#DateOfBirthMonth').select('August')        
+        cy.get('#DateOfBirthYear').select('1997')
+        cy.get("#Email").type(faker.internet.email({ firstName: 'marta', lastName: 'doe', provider: 'happyprovider.com'}))
+        cy.get('#Username').type(faker.internet.username({ firstName: 'martha', lastName: 'doe'}))
+        cy.get("#Password").type("123456")
+        cy.get("#ConfirmPassword").type("123456")
+        cy.get('#Company').type('Company name')
+
+
+        cy.get('#register-button').click()
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/registerresult/1')
+        cy.contains('p', 'Your registration completed')
+        })
+
+      
 
 })
