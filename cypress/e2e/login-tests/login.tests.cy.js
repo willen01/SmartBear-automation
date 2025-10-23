86979c05d0a73e16bde4a7221bedabfdd8331270
willen01 <willen.sesantos@gmail.com>
2025-10-23 14:04:47 -0300

    test: Valida login com credenciais incorretas e formulário vazio

--- cypress/e2e/login-tests/login.tests.cy.js
+++ cypress/e2e/login-tests/login.tests.cy.js
@@ -4,13 +4,31 @@ describe('validar login de usuário', () => {
     });
 
 
-    it('Login com sucesso', () => {
+    it('Login de usuário com dados válidoso', () => {
         cy.get('#UsernameOrEmail').type('jhonDoe01')
         cy.get('#Password').type('q1w2e3')    
         cy.get('.btn-login').click()
 
         cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/')
         cy.contains('Welcome to our store.').should('be.visible')
+    })
+
+    it('Login de usuário com credenciais incorretas', () => {
+        cy.get('#UsernameOrEmail').type('jhonDoe01')
+        cy.get('#Password').type('q1w3')    
+        cy.get('.btn-login').click()
+
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/login')
+        cy.contains('The credentials provided are incorrect').should('be.visible')
+    })
+
+    it('Login de usuário com formulário vazio', () => {
+        cy.get('#UsernameOrEmail')
+        cy.get('#Password') 
+        cy.get('.btn-login').click()
+
+        cy.url().should('eq', 'https://bearstore-testsite.smartbear.com/login')
+        cy.contains('The credentials provided are incorrect').should('be.visible')
     })
     
 })
